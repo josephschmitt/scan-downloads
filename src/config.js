866e0db510fd7a28a598494aeab740264105da8a
Joseph Schmitt <me@joe.sh>
2017-05-22 19:54:31 -0400

    Support starting multiple apps by providing a comma-list of app names.

--- src/config.js
+++ src/config.js
@@ -2,7 +2,14 @@ import config from 'node-confenv';
 import path from 'path';
 import args from './lib/cliArgs'
 
-export default function(app=args.app) {
+/**
+ * Reads the configuration file for a particular app.
+ *
+ * @param {String} [app] -- Application to load the config for. If none is provided, it'll, try
+ *     reading a generic .env file instead.
+ * @returns {Object} -- Object with the configuration settings for the app.
+ */
+export default function(app) {
   const filename = `.env${app ? '.' + app : ''}`;
   config.read(filename);
 
--- src/index.js
+++ src/index.js
@@ -5,12 +5,25 @@ import args from './lib/cliArgs.js';
 import couchpotato from './lib/couchpotato.js';
 import sonarr from './lib/sonarr.js';
 
-const {app} = args;
-
-if (app === 'couchpotato') {
-  couchpotato();
-} else if (app === 'sonarr') {
-  sonarr();
-} else {
-  console.error('Invalid app name:', chalk.cyan(app));
+/**
+ * Run the completed downloads scan for a specific app.
+ *
+ * @param {String} app -- Name of the app to run the scan for.
+ */
+async function scan(app) {
+  try {
+    if (app === 'couchpotato') {
+      await couchpotato();
+    } else if (app === 'sonarr') {
+      await sonarr();
+    } else {
+      console.error('Invalid app name:', chalk.cyan(app));
+    }
+    console.log(chalk.green('Success!'), chalk.cyan(app), 'scan queued.');
+  } catch (e) {
+    console.error(chalk.red('Error') + ':', chalk.cyan(app), e.message);
+  }
 }
+
+// Run scanner for the app or app(s)
+(args.app || '').split(',').forEach((app) => scan(app.trim()));
